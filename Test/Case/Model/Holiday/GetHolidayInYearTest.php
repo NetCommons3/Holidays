bff44ad82ce2f740212e5c767de1d2c0298c5236
s-nakajima <nakajimashouhei@gmail.com>
2016-01-17 06:56:06 +0900

    site_settingsのMigration、Model等をSiteManagerに移動する https://github.com/NetCommons3/NetCommons3/issues/104

--- Test/Case/Model/Holiday/GetHolidayInYearTest.php
+++ Test/Case/Model/Holiday/GetHolidayInYearTest.php
@@ -34,7 +34,7 @@ class HolidayGetHolidayInYearTest extends NetCommonsGetTest {
  * @var array
  */
 	public $fixtures = array(
-		'plugin.net_commons.site_setting',
+		'plugin.site_manager.site_setting',
 		'plugin.holidays.holiday',
 	);
 
