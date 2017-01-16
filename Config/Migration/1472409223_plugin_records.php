e672b3d6c066562228e5438d437361000b454eaa
s-nakajima <nakajimashouhei@gmail.com>
2017-01-16 17:34:37 +0900

    Pluginデータ修正 https://github.com/NetCommons3/NetCommons3/issues/770

--- Config/Migration/1472409223_plugin_records.php
+++ Config/Migration/1472409223_plugin_records.php
@@ -45,6 +45,8 @@ class PluginRecords extends NetCommonsMigration {
 			//日本語
 			array(
 				'language_id' => '2',
+				'is_origin' => true,
+				'is_translation' => true,
 				'key' => 'holidays',
 				'namespace' => 'netcommons/holidays',
 				'name' => '祝日設定',
@@ -57,6 +59,8 @@ class PluginRecords extends NetCommonsMigration {
 			//英語
 			array(
 				'language_id' => '1',
+				'is_origin' => false,
+				'is_translation' => true,
 				'key' => 'holidays',
 				'namespace' => 'netcommons/holidays',
 				'name' => 'Holidays',
